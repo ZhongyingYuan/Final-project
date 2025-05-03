74daa5e6ce375487483f7631acc87690c7e2eeef
GraceCen1234 <gracexinrancen@gmail.com>
2025-05-03 22:06:13 +1000

    Add files via upload
    Project

---
+++ ToDoListApp/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by grace cen on 3/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
