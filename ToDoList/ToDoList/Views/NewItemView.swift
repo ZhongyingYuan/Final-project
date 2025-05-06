d4410911cae25d03234d2710889f6243778ad513
GraceCen1234 <gracexinrancen@gmail.com>
2025-05-06 22:02:39 +1000

    Update NewItemView.swift

--- ToDoList/ToDoList/Views/NewItemView.swift
+++ ToDoList/ToDoList/Views/NewItemView.swift
@@ -3,16 +3,99 @@
 //  ToDoList
 //
 //  Created by 袁钟盈 on 2025/5/4.
-//
+//edited by Grace
 
 import SwiftUI
 
 struct NewItemView: View {
+    @StateObject var viewModel = NewItemViewViewModel()
+    @Binding var newItemPresented : Bool
+    
+    @State private var color : Color = .blue
+    @State private var date = Date.now
+    let daysOfTheWeek = Date.capitalizedFirstLettersOfWeekdays
+    let columns = Array(repeating: GridItem(.flexible()), count: 7)
+    //let days = 1..<32
+    @State private var days: [Date] = []
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("New Item").font(.system(size: 32)).bold().padding()
+            Form {
+                //Textfield
+                TextField("Title", text: $viewModel.title ).textFieldStyle(DefaultTextFieldStyle())
+                
+                LabeledContent("Calendar colour") {
+                    ColorPicker("", selection: $color, supportsOpacity: false)
+                }
+                //Due Date
+                DatePicker("Due Date", selection: $viewModel.dueDate).datePickerStyle(GraphicalDatePickerStyle())
+                
+                TLButton(title: "Save", background: .pink) {
+                    if viewModel.canSave {
+                        viewModel.save()
+                        newItemPresented = false
+                    }
+                    else {
+                        viewModel.showAlert = true
+                    }
+                }
+                .padding()
+                
+            } //error message to alert user if something went wrong
+            .alert(isPresented: $viewModel.showAlert){
+                Alert(title: Text("Error"), message: Text("Please fill in all the fields and select due date"))
+            }
+//            LabeledContent("Date/Time") {
+//                DatePicker("", selection: $date)
+//            }
+//            HStack {
+//                ForEach(daysOfTheWeek.indices, id: \.self) {
+//                    index in
+//                    Text(daysOfTheWeek[index])
+//                        .fontWeight(.black)
+//                        .foregroundStyle(color)
+//                        .frame(maxWidth: .infinity)
+//                }
+//            }
+//            LazyVGrid(columns: columns) {
+//                ForEach(days, id: \.self ) { day in
+//                    if day.monthInt != date.monthInt {
+//                        Text("")
+//                    }
+//                    else {
+//                        Text(day.formatted(.dateTime.day()))
+//                            .fontWeight(.bold)
+//                            .foregroundStyle(.secondary)
+//                            .frame(maxWidth: .infinity, minHeight: 40)
+//                            .background(
+//                                Circle()
+//                                    .foregroundStyle(
+//                                        //color.opacity(0.3)
+//                                        Date.now.startOfDay == day.startOfDay
+//                                        ? .red.opacity(0.3)
+//                                        :  color.opacity(0.3)
+//                                    )
+//                            )
+//                    }
+//                    
+//                }
+//            }
+//        }
+//        .padding()
+//        .onAppear {
+//            days = date.calendarDisplayDays
+//        }
+//        .onChange(of: date) {
+//            days = date.calendarDisplayDays
+        }
     }
 }
 
+
 #Preview {
-    NewItemView()
+    NewItemView(newItemPresented: Binding(get: {
+        return true
+    }, set: { _ in 
+        
+    }))
 }
