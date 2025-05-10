65789e3fa586ddaf9030c9b331e0fb6cae25ddec
GraceCen1234 <gracexinrancen@gmail.com>
2025-05-10 23:45:38 +1000

    Add files via upload

---
+++ ToDoList/ToDoList/Others/ExtDate.swift
@@ -0,0 +1,106 @@
+//
+//  ExtDate.swift
+//  Calendar example
+//
+//  Created by grace cen on 2/5/2025.
+//
+
+import Foundation
+
+extension Date {
+    static var firstDayOfWeek = Calendar.current.firstWeekday
+    
+    static var capitalizedFirstLettersOfWeekdays: [String] {
+        let calendar = Calendar.current
+        // Adjusted for the different weekday starts
+        var weekdays = calendar.shortWeekdaySymbols
+        if firstDayOfWeek > 1 {
+            for _ in 1..<firstDayOfWeek {
+                if let first = weekdays.first {
+                    weekdays.append(first)
+                    weekdays.removeFirst()
+                }
+            }
+        }
+        return weekdays.map { $0.capitalized }
+    }
+    
+    var startOfMonth: Date {
+        Calendar.current.dateInterval(of: .month, for: self)!.start
+    }
+    
+    var endOfMonth: Date {
+        let lastDay = Calendar.current.dateInterval(of: .month, for: self)!.end
+        return Calendar.current.date(byAdding: .day, value: -1, to: lastDay)!
+    }
+    
+    var numberOfDaysInMonth: Int {
+        Calendar.current.component(.day, from: endOfMonth)
+    }
+    
+    var firstWeekDayBeforeStart: Date {
+       let startOfMonthWeekday = Calendar.current.component(.weekday, from: startOfMonth)
+       var numberFromPreviousMonth = startOfMonthWeekday - Self.firstDayOfWeek
+       if numberFromPreviousMonth < 0 {
+           numberFromPreviousMonth += 7 // Adjust to a 0-6 range if negative
+       }
+       return Calendar.current.date(byAdding: .day, value: -numberFromPreviousMonth, to: startOfMonth)!
+    }
+    
+    var calendarDisplayDays: [Date] {
+       var days: [Date] = []
+       // Start with days from the previous month to fill the grid
+       let firstDisplayDay = firstWeekDayBeforeStart
+       var day = firstDisplayDay
+       while day < startOfMonth {
+           days.append(day)
+           day = Calendar.current.date(byAdding: .day, value: 1, to: day)!
+       }
+       // Add days of the current month
+       for dayOffset in 0..<numberOfDaysInMonth {
+           if let newDay = Calendar.current.date(byAdding: .day, value: dayOffset, to: startOfMonth) {
+               days.append(newDay)
+           }
+       }
+       return days
+    }
+    
+    var monthInt: Int {
+        Calendar.current.component(.month, from: self)
+    }
+    
+    var startOfDay: Date {
+        Calendar.current.startOfDay(for: self)
+    }
+    
+    var hourInt: Int {
+        Calendar.current.component(.hour, from: self)
+    }
+    
+    var minuteInt: Int {
+        Calendar.current.component(.minute, from: self)
+    }
+    
+    var formattedDate: String {
+        let formatter = ISO8601DateFormatter()
+        formatter.timeZone = .current
+        formatter.formatOptions = [.withFullDate]
+        return formatter.string(from: self)
+    }
+    
+    var formattedDateHourCombined: String {
+        let formatter = ISO8601DateFormatter()
+        formatter.timeZone = .current
+        return formatter.string(from: self)
+    }
+    
+    // Used to generate the mock data for previews
+    // Computed property courtesy of ChatGPT
+    var randomDateWithinLastThreeMonths: Date {
+        let threeMonthsAgo = Calendar.current.date(byAdding: .month, value: -3, to: self)!
+        let randomTimeInterval = TimeInterval.random(in: 0.0..<self.timeIntervalSince(threeMonthsAgo))
+        let randomDate = threeMonthsAgo.addingTimeInterval(randomTimeInterval)
+        return randomDate
+    }
+
+}
