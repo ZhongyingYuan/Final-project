f4cf6298101f004598bce22a3a58d05b7e02d156
Margarita5D <margarita.dowd@student.uts.edu.au>
2025-05-11 19:34:31 +1000

    Update ToDoListViewViewModel.swift

--- ToDoList/ToDoList/ViewModels/ToDoListViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/ToDoListViewViewModel.swift
@@ -5,8 +5,25 @@
 //  Created by 袁钟盈 on 2025/5/4.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
-class ToDoListViewViewModel: ObservableObject {
-    @Published var showingNewItemView = false
+
+class ToDoListItemViewViewModel: ObservableObject {
     init() {}
-}
+
+    func toggleIsCompleted(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.toggleCompleted(!item.isCompleted)
+        guard Auth.auth().currentUser?.uid != nil else {
+            return
+        }
+    
+        let db = Firestore.firestore()
+        _ = db.collection("users")
+            .document("userid")
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(from: itemCopy)
+        }
+    }
