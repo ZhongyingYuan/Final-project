f02af40039724340ae0844ae690add01fc7f2c94
Margarita5D <margarita.dowd@student.uts.edu.au>
2025-05-11 19:37:09 +1000

    Update Profile View.swift

--- ToDoList/ToDoList/Views/Profile View.swift
+++ ToDoList/ToDoList/Views/Profile View.swift
@@ -7,12 +7,65 @@
 
 import SwiftUI
 
-struct Profile_View: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewViewModel()
+    
+    var body: some View{
+        NavigationView{
+            VStack {
+            }
+        }
+        NavigationView {
+            VStack {
+                if let user = viewModel.user {
+                    // avatar
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                        .frame(width: 125, height: 125)
+                        .padding()
+                    // user name and email
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Name: ")
+                                .bold()
+                            Text(user.name)
+                        }
+                        HStack {
+                            Text("Email: ")
+                                .bold()
+                            Text(user.email)
+                        }
+                        .padding()
+                    }
+                    .padding()
+                    
+                    
+                    // sign out
+                    Button("Log Out") {
+                        viewModel.logOut()
+                    }
+                    tint(.red)
+                        .padding()
+                    
+                    Spacer()
+                    
+                } else {
+                    Text("Loading Profile...")
+                }
+            }
+            navigationTitle("Profile")
+        }
+        .onAppear {
+            viewModel.fetchUser()
+            
+        }
     }
 }
 
-#Preview {
-    Profile_View()
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
 }
